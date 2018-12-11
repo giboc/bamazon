b4afd6d2283eab47581846b079df246b7fa87fcc
Alex Sato <as4628@columbia.edu>
2018-12-10 21:46:40 -0500

    Fully functional. Does not do any input checking.

---
+++ bamazonCustomer.js
@@ -0,0 +1,69 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "aVarice1234!",
+    database: "bamazon"
+});
+
+
+
+con.connect(function (err) {
+    if (err)
+        console.log(err);
+    console.log("Connected!");
+});
+
+var sql = "select * from products;"
+
+con.query(sql, function (err, result, fields) {
+    if (err)
+        console.log(err);
+    result.forEach(function (row) {
+        console.log("ID: " + row.item_id);
+        console.log("Item: " + row.product_name);
+        console.log("Price: $" + row.price);
+        console.log("");
+    });
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: "item_id",
+            message: "What product (id) do you want to buy?"
+        },
+        {
+            type: 'input',
+            name: "quantity",
+            message: "How many do you want?"
+        }
+    ])
+    .then(answers => {
+        console.log(answers.item_id);
+        console.log(answers.quantity);
+        sql = "select stock_quantity from products where item_id = \"" + answers.item_id + "\"";
+        con.query(sql, (function (err, row, field){
+            if(row[0].stock_quantity >= answers.quantity){
+                sql = "UPDATE products SET stock_quantity = stock_quantity - " + answers.quantity + " WHERE item_id = \""+answers.item_id+"\"";
+                con.query(sql, function(err, result){
+                    if(err)
+                        console.log(err)
+                    console.log("Order placed!");
+                    sql = "SELECT price from products where item_id= \""+ answers.item_id + "\"";
+                    con.query(sql, (function(err, row, field){
+                        console.log("Total price of your order: $" + row[0].price*answers.quantity); 
+                    }))
+                });
+            }
+            else
+                console.log("Insufficient quantity. Cancelling order.");
+                
+            
+            
+        }));
+        
+    });
+});
+
